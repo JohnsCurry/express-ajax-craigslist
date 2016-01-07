3c87bc6c83538a6e0c502b0e7c93476d1c165059
John Curry <john.steven.curry@gmail.com>
2016-01-06 18:33:31 -0800

    comments incluced code lists all results but has no dynamic link naming

--- app.js
+++ app.js
@@ -27,13 +27,48 @@ app.use(express.static(path.join(__dirname, 'public')));
 //app.use('/users', users);
 
 app.get('/', function(req, res) {
-  res.render('index')
+  res.render('index') //renders the index.jade template when routing to /
 });
 
+
+
+app.get('/searching', function(req, res){
+
+  //input value from search
+  var val = req.query.search;
+  //console.log(val);
+  //res.send(val); // Pretty sure this returns what you type into the text field
+
+  // url used to search yql
+  var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search" +
+  "%20where%20location%3D%22sfbay%22%20and%20type%3D%22jjj%22%20and%20query%3D%22" + val + "%22&format=" +
+  "json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys";
+  console.log("Stop printing me");
+
+  // request module is used to process the yql url and return the results in JSON format
+  request(url, function(err, resp, body) {
+    console.log("I am undefined" + body.query);
+    body = JSON.parse(body);
+    //ThIS LINE BELOW IS IMPORTANT. For some reason I couldn't traverse the object until now.
+    console.log(body.query.results); // This is a big object
+    // logic used to compare search results with the input from user
+    if (!body.query.results.RDF.item) {
+      craig = "No results found. Try again.";
+    } else {
+      //craig = body.query.results.RDF.item[0]['about'];
+      craig = body.query.results.RDF.item;
+    }
+  // pass back the results to client side
+    res.send(craig);
+  });
+
+});
+
+
+/*
 app.get('/searching', function(req, res){
   //input from search
   var val = req.query.search;
-  //console.log(val + " is the value");
   //res.send("WHEEE This just tests the /searching route");
   var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20craigslist.search" +
   "%20where%20location%3D%22sfbay%22%20and%20type%3D%22jjj%22%20and%20query%3D%22" + val + "%22&format=" +
@@ -41,10 +76,8 @@ app.get('/searching', function(req, res){
   
   //request module is used to process the yql url and return the results in JSON format
   request(url, function(err, resp, body){
-    //console.log("Before: " + body);
     body = JSON.parse(body);
     // logic used to compare search results with the input from user
-    //console.log("After: \n\n\n\n " + body);
     if (!body.query.results.RDF.item) {
       craig = "No results found. Try again.";
     } else {
@@ -54,6 +87,9 @@ app.get('/searching', function(req, res){
   //console.log(url);
   res.send(craig);
 });
+*/
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,7 +3,10 @@ $(function(){
     if(e.keyCode === 13) {
       var parameters = {search: $(this).val() };
       $.get( '/searching', parameters, function(data) {
-        $('#results').html(data);
+        data.forEach(function(item){
+          $('#results').append('<a href="' + item['about'] + '">Craigslist Job</a><br>');
+        });
+        //$('#results').html();
       });
     };
   });
